7f53e9d0d85e9f55aca26ed0447c4595fd9489bf
vorou <voropaev.viktor@gmail.com>
2017-07-12 21:48:54 +0500

    fdhf

---
+++ perc/percolation/src/Percolation.java
@@ -0,0 +1,35 @@
+public class Percolation {
+    // create n-by-n grid, with all sites blocked
+    public Percolation(int n) {
+    }
+
+    // open site (row, col) if it is not open already
+    public void open(int row, int col) {
+
+    }
+
+    // is site (row, col) open?
+    public boolean isOpen(int row, int col) {
+
+    }
+
+    // is site (row, col) full?
+    public boolean isFull(int row, int col) {
+
+    }
+
+    // number of open sites
+    public int numberOfOpenSites() {
+
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+
+    }
+
+    // test client (optional)
+    public static void main(String[] args) {
+        Percolation percolation = new Percolation(10);
+    }
+}
